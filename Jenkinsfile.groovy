a8fe06a8e92f53ecb9b7be124f4f297566a7225e
katharxix <kchavesb@ucentral.edu.co>
2020-08-18 17:28:12 -0500

    put a job inside the folder

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,27 +2,24 @@ String basePath = '/var/jenkins_home/workspace'
 String repo = 'git@github.com:katharxix/test-jenkins.git'
 String credentialssh = 'PersonalGithub'
 
-    folder {basePath} {
+    folder ('Job as Code'){
         description ('folder for practice excercises')
         displayName ('Jobs DSL folder')
-        configure { folder -> folder / icon (class: 'localhost:8080.MyFolderIcon')}
     }
 
-pipelineJob('Job as Code'){
-    job("${basePath}/example-build') 
-    scm {
-        git{
-            remote{
-                url ("${repo}")
-                credentials ("${credentialssh}")
+    job('Job as Code/my pipeline'){
+        scm {
+            git{
+               remote{
+                   url ("${repo}")
+                   credentials ("${credentialssh}")
+               }   
             branches ("master")
             }
         }
+        
     }
-    
-}
-    sandbox(boolean sandbox = true)
-credentials('String credentialsgithub')
 
+    
 
     
